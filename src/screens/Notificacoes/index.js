9ff1af1084b580a34ea95204ad6ca967c55d8710
Alexandre de Souza Jr <souzajr.alexandre@gmail.com>
2023-02-15 18:14:13 -0300

    Criação de mensagem no caso de não haver notificações

--- src/screens/Notificacoes/index.js
+++ src/screens/Notificacoes/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { FlatList, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header";
 import NotificationCard from "../../components/NotificationCard";
 import { NotificacaoContext } from "../../contexts/NotificacaoContext";
@@ -8,19 +8,42 @@ const Notificacoes = () => {
     const { notificacoes, dispensarNotificacao } = useContext(NotificacaoContext)
 
     return (
-        <FlatList
-            data={notificacoes}
-            renderItem={({ item }) => <NotificationCard notificacao={item} onPress={() => dispensarNotificacao(item.id)} />}
-            keyExtractor={item => item.id}
-            ListHeaderComponent={() => {
-                return (
-                    <View style={{ marginBottom: 25 }}>
-                        <Header title={"Notificações"} />
-                    </View>
-                )
-            }}
-        />
+        notificacoes?.length > 0 ? (
+            <FlatList
+                data={notificacoes}
+                renderItem={({ item }) => <NotificationCard notificacao={item} onPress={() => dispensarNotificacao(item.id)} />}
+                keyExtractor={item => item.id}
+                ListHeaderComponent={() => {
+                    return (
+                        <View style={{ marginBottom: 25 }}>
+                            <Header title={"Notificações"} />
+                        </View>
+                    )
+                }}
+            />
+        ) : (
+            <View style={{ marginBottom: 25 }}>
+                <Header title={"Notificações"} />
+                <View style={estilos.container}>
+                    <Text style={estilos.texto}>Não há notificações no momento.</Text>
+                </View>
+            </View>
+        )
+
     )
 }
 
 export default Notificacoes;
+
+const estilos = StyleSheet.create({
+    container: {
+        marginHorizontal: 25,
+        marginTop: 25,
+        marginBottom: 15
+    },
+    texto: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#000",
+    },
+});
